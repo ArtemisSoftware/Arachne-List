9632e5409c0e9683bb57f4b5491a08efedb26d52
gustavo <gustavo.isaac@gmail.com>
2021-08-20 12:48:33 +0100

    Connect ItemListAdapter to fragment

--- app/src/main/java/com/artemissoftware/arachnelist/fragments/listadapter/ListAdapterFragment.kt
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/listadapter/ListAdapterFragment.kt
@@ -3,8 +3,11 @@ package com.artemissoftware.arachnelist.fragments.listadapter
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.artemissoftware.arachnelist.MainActivity
 import com.artemissoftware.arachnelist.R
 import com.artemissoftware.arachnelist.databinding.FragmentListAdapterBinding
+import com.artemissoftware.arachnelist.fragments.listadapter.adapters.ItemListAdapter
 
 
 class ListAdapterFragment : Fragment(R.layout.fragment_list_adapter) {
@@ -12,11 +15,23 @@ class ListAdapterFragment : Fragment(R.layout.fragment_list_adapter) {
     private var _binding: FragmentListAdapterBinding? = null
     private val binding get() = _binding!!
 
+    private val itemListAdapter by lazy { ItemListAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
         _binding = FragmentListAdapterBinding.bind(view)
 
+        setupRecyclerView()
+
+        itemListAdapter.submitList((activity as MainActivity).generateDummyList(7))
 
     }
 
+    private fun setupRecyclerView() {
+
+        binding.rclyListAdapter.apply {
+            adapter = itemListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
 }
