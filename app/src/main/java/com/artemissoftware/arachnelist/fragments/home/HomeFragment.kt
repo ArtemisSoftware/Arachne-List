1c7552c7ea16260653385093f2fd7ae2428ace9e
gustavo <gustavo.isaac@gmail.com>
2021-08-20 14:15:48 +0100

    HomeFragment

--- app/src/main/java/com/artemissoftware/arachnelist/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/home/HomeFragment.kt
@@ -5,12 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.artemissoftware.arachnelist.R
+import com.artemissoftware.arachnelist.databinding.FragmentHomeBinding
+import com.artemissoftware.arachnelist.databinding.FragmentMultipleBinding
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        _binding = FragmentHomeBinding.bind(view)
+
+
+        binding.btnSimpleRecycler.setOnClickListener {
+
+            findNavController().navigate(R.id.action_homeFragment_to_simpleFragment)
+
+        }
+
+        binding.btnListAdapter.setOnClickListener {
+
+            findNavController().navigate(R.id.action_homeFragment_to_listAdapterFragment)
+
+        }
+
+        binding.btnMultipleAdapter.setOnClickListener {
+
+            findNavController().navigate(R.id.action_homeFragment_to_multipleFragment)
+
+        }
     }
 }
