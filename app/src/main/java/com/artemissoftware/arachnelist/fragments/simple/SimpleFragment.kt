8be0e6e6355ca8047ccdc15b09ebd0272c3685c8
gustavo <gustavo.isaac@gmail.com>
2021-08-20 11:36:09 +0100

    SimpleFragment.kt

---
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/simple/SimpleFragment.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.arachnelist.fragments.simple
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.arachnelist.R
+
+
+class SimpleFragment : Fragment(R.layout.fragment_simple) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
