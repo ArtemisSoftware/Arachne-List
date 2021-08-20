36df0fe17a1531d37a7b26aa12f362ec34daaf46
gustavo <gustavo.isaac@gmail.com>
2021-08-20 11:51:05 +0100

    SimpleAdapter

---
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/simple/adapters/SimpleAdapter.kt
@@ -0,0 +1,60 @@
+package com.artemissoftware.arachnelist.fragments.simple.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.artemissoftware.arachnelist.R
+import com.artemissoftware.arachnelist.models.Item
+import kotlinx.android.synthetic.main.item_simple.view.*
+
+class SimpleAdapter (private val itemList: List<Item>/*, private val listener: OnItemClickListener*/) : RecyclerView.Adapter<SimpleAdapter.SimpleViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
+
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_simple, parent, false)
+        return SimpleViewHolder(itemView)
+    }
+
+
+
+    override fun onBindViewHolder(holder: SimpleViewHolder, position: Int) {
+
+        val currentItem = itemList[position]
+        holder.imageView.setImageResource(currentItem.imageResource)
+        holder.textView1.text = currentItem.text1
+        holder.textView2.text = currentItem.text2
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    inner class SimpleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        val imageView : ImageView = itemView.image_view
+        val textView1: TextView = itemView.text_view_1
+        val textView2: TextView = itemView.text_view_2
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                //--listener.onItemClick(position)
+            }
+        }
+    }
+
+//    interface OnItemClickListener{
+//        fun onItemClick(position : Int)
+//    }
+
+}
