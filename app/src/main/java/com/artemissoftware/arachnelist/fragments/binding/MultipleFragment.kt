c7f8f5b4dee6458b89505d8bfc60de346f3f6ff1
gustavo <gustavo.isaac@gmail.com>
2021-08-20 13:51:18 +0100

    MultipleFragment.kt

---
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/binding/MultipleFragment.kt
@@ -0,0 +1,40 @@
+package com.artemissoftware.arachnelist.fragments.binding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.artemissoftware.arachnelist.MainActivity
+import com.artemissoftware.arachnelist.R
+import com.artemissoftware.arachnelist.databinding.FragmentListAdapterBinding
+import com.artemissoftware.arachnelist.databinding.FragmentMultipleBinding
+import com.artemissoftware.arachnelist.fragments.listadapter.adapters.ItemListAdapter
+
+
+class MultipleFragment : Fragment(R.layout.fragment_multiple) {
+
+    private var _binding: FragmentMultipleBinding? = null
+    private val binding get() = _binding!!
+
+    private val itemListAdapter by lazy { ItemListAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        _binding = FragmentMultipleBinding.bind(view)
+
+        setupRecyclerView()
+
+        itemListAdapter.submitList((activity as MainActivity).generateDummyList(2))
+
+    }
+
+    private fun setupRecyclerView() {
+
+        binding.rclyMultipleAdapter.apply {
+            adapter = itemListAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+}
