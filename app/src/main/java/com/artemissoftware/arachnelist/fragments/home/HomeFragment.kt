b1d511dd4679e521ef31990f3d482c41066b6dc6
gustavo <gustavo.isaac@gmail.com>
2021-08-20 11:18:30 +0100

    HomeFragment.kt

---
+++ app/src/main/java/com/artemissoftware/arachnelist/fragments/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.artemissoftware.arachnelist.fragments.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.arachnelist.R
+
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
